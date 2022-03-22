99874d7701ea48248c6c59505525548830cdef00
Henrique <henrique.campana22@gmail.com>
2022-03-22 17:28:33 -0300

    Cria os testes para o componente about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Tests component <About />', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+  });
+
+  test('Tests if Pokédex info is present in the page', () => {
+    const aboutText = screen.getByText(/This application simulates/i);
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('Tests if there is a heading "h2" with the text "About Pokédex."', () => {
+    const headerPokedex = screen.getByText(/About Pokédex/i);
+    expect(headerPokedex).toBeInTheDocument();
+  });
+
+  test('Tests if there is a Pokédex image in the page', () => {
+    const IMG_SRC = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const img = screen.getByRole('img');
+    expect(img.src).toEqual(IMG_SRC);
+  });
+});
